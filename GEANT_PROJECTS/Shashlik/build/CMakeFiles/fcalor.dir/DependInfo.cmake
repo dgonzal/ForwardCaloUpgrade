1b40387529f60eaf8b70d9e98a66b03b4e0c085f
Daniel <daniel.gonzalez@desy.de>
2014-06-19 11:49:07 +0200

    try cmake

---
+++ GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/fcalor.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/fcalor.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/DetectorConstruction.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/DetectorConstruction.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/DetectorMessenger.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/DetectorMessenger.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/EventAction.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/EventAction.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HEPEvtcom.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HEPEvtcom.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HepMCG4AsciiReader.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HepMCG4AsciiReader.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HepMCG4AsciiReaderMessenger.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HepMCG4AsciiReaderMessenger.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HepMCG4Interface.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HepMCG4Interface.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HistoManager.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HistoManager.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/HistoMessenger.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/HistoMessenger.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/PrimaryGeneratorAction.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/PrimaryGeneratorAction.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/PrimaryGeneratorActionMessenger.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/PrimaryGeneratorActionMessenger.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/RunAction.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/RunAction.cc.o"
+  "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/src/SteppingAction.cc" "/afs/desy.de/user/g/gonvaq/xxl-af-cms/ForwardCaloUpgrade/GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/src/SteppingAction.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4_STORE_TRAJECTORY"
+  "G4VERBOSE"
+  "G4UI_USE"
+  "G4VIS_USE"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ GEANT_PROJECTS/Shashlik/build/CMakeFiles/fcalor.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fcalor.dir/fcalor.cc.o"
+  "CMakeFiles/fcalor.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/fcalor.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/fcalor.dir/src/EventAction.cc.o"
+  "CMakeFiles/fcalor.dir/src/HEPEvtcom.cc.o"
+  "CMakeFiles/fcalor.dir/src/HepMCG4AsciiReader.cc.o"
+  "CMakeFiles/fcalor.dir/src/HepMCG4AsciiReaderMessenger.cc.o"
+  "CMakeFiles/fcalor.dir/src/HepMCG4Interface.cc.o"
+  "CMakeFiles/fcalor.dir/src/HistoManager.cc.o"
+  "CMakeFiles/fcalor.dir/src/HistoMessenger.cc.o"
+  "CMakeFiles/fcalor.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/fcalor.dir/src/PrimaryGeneratorActionMessenger.cc.o"
+  "CMakeFiles/fcalor.dir/src/RunAction.cc.o"
+  "CMakeFiles/fcalor.dir/src/SteppingAction.cc.o"
+  "fcalor.pdb"
+  "fcalor"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fcalor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
